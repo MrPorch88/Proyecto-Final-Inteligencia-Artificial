2f62033860d5041b8d085fb9fbefa81dd406f1ae
Jorge Moreno <jorge.velez.moreno@gmail.com>
2020-01-14 12:32:07 +0100

    Cambios iniciales
    Preparado para iniciar parseo, Gallego.
    
    Co-authored-by: Ignacio Triguero <itrigueropedraza@gmail.com>
    Co-authored-by: Julián de Pablo <38203898+julidpt@users.noreply.github.com>

--- principal.py
+++ principal.py
@@ -1,9 +1,32 @@
 import librosa
+import numpy as np
 import soundfile
-import numpy
+import pyaudio
+import os
+#[
+# {
+# ruta:'ruta',
+# emocion: 'alegria'
+# },
+# ruta:'ruta',
+# emocion: 'tristeza'
+# },etc etc
+# ]
+# Metodo de parseo de datos
+def parseo_datos():
+    entradas = os.listdir('data/')
+    for entrada in entradas:
+        print(entrada) # Deberia de imprimir el nombre de cada archivo de la carpeta principal de audios
 
+
+
+# Metodo para seleccionar archivos de audio
+
+
+# Metodo para extraer las caracteristicas del audio en cuestion. MFCCs
 def extract_feature(file_name):
     with soundfile.SoundFile(file_name) as sound_file:
         X, sample_rate = librosa.load(file_name, mono=True)
-        Mfccs_mean = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=100).T, axis=0)
+        mfccs_mean = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=100).T, axis=0)
     return mfccs_mean
+
