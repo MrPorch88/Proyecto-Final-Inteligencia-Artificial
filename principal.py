742baa8856255e03cbb56a131c8ac537d25d42f8
Ignacio Triguero <itrigueropedraza@gmail.com>
2020-01-25 18:45:50 +0100

    Terminado sin comentarios
    Neural network hidden layer size = 100, .... a falta de pruebas

--- principal.py
+++ principal.py
@@ -17,21 +17,41 @@ def parseo_datos(direc):
     for root, dirs, files in os.walk(direc):
         for name in files:
             emotion = name.split('_')
-            if not root.endswith('Test1') and not root.endswith('Test2'):
-                try:
-                    features = extract_feature( root + '/' + name)
-                    X.append(features)
-                    y.append(emotion[0])
-                except:
-                    print(root + '/' + name + ' - Falla')
-                    continue
+            if name.endswith('.WAV'):
+                if not root.endswith('Test1') and not root.endswith('Test2'):
+                    if emotion[0] is not 'Alegria' and not 'Asco' and not 'Ira' and not 'Miedo' and not 'Neutro' and not 'Sorpresa' and not 'Tristeza':
+                        continue
+                    try:
+                        features = extract_feature( root + '/' + name)
+                        X.append(features)
+                        y.append(emotion[0])
+                    except:
+                        print(root + '/' + name + ' - Falla')
+                        continue
 
     return train_test_split(np.array(X), y)
+
+def getTestAudios(direc):
+    X = []
+    y = []
+    for root, dirs, files in os.walk(direc):
+        for name in files:
+            emotion = name.split('_')
+            if name.endswith('.WAV'):
+                if root.endswith('Test1') or root.endswith('Test2'):
+                    try:
+                        features = extract_feature( root + '/' + name)
+                        X.append(features)
+                        y.append(emotion[0])
+                    except:
+                        print(root + '/' + name + ' - Falla')
+                        continue
+    return X, y
 
 # Metodo para extraer las caracteristicas del audio en cuestion. MFCCs
 def extract_feature(file_name):
        
-    with sf.SoundFile(file_name) as sound_file:
+    with sf.SoundFile(file_name):
         X, sample_rate = librosa.load(file_name, mono=True)
         mfccs = librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=100).T
         mfccs_mean = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=100).T, axis=0)
@@ -42,33 +62,41 @@ def extract_feature(file_name):
     return mfccs
 
 # Metodo de Redes neuronales con los argumentos:
-# hidden_layer_sizes=(100,), verbose=True, max_iter=2000, learning_rate='constant'
-def neuralNetwork100_0(X_train, X_test, y_train, y_test):
-    neuralNetwork = MLPClassifier(hidden_layer_sizes=(100,100,100), verbose=True, max_iter=2000, learning_rate='adaptive')
+def neuralNetwork(X_train, X_test, y_train, y_test, x_final_test, y_final_test):
+    neuralNetwork = MLPClassifier(hidden_layer_sizes=(100,), max_iter=2000, learning_rate='adaptive')
     neuralNetwork.fit(X_train, y_train)
     y_pred = neuralNetwork.predict(X_test)
-    print("Neural Network. Argumentos: hidden_layer_sizes=(100,), verbose=True, max_iter=2000, learning_rate='constant'")
-    print(neuralNetwork.get_params)
+    print("Neural Network")
+    print("Test de aprendizaje")
     print(confusion_matrix(y_test, y_pred))
     print(accuracy_score(y_test, y_pred))
+    y_final_pred = neuralNetwork.predict(x_final_test)
+    print("Test con las carpetas Test")
+    print(confusion_matrix(y_final_test, y_final_pred))
+    print(accuracy_score(y_final_test, y_final_pred))
 
 
 # Metodo de Bayes 
-def naiveBayes(X_train, X_test, y_train, y_test):
+def naiveBayes(X_train, X_test, y_train, y_test, x_final_test, y_final_test):
     classifier = GaussianNB()
     classifier.fit(X_train, y_train)
     y_pred = classifier.predict(X_test)
     print("Gaussian Naive Bayes")
+    print("Test de aprendizaje")
     print(confusion_matrix(y_test, y_pred))
     print(accuracy_score(y_test, y_pred))
+    y_final_pred = classifier.predict(x_final_test)
+    print("Test con las carpetas Test")
+    print(confusion_matrix(y_final_test, y_final_pred))
+    print(accuracy_score(y_final_test, y_final_pred))
+    print()
 
-def ejecucionModelos(X_train, X_test, y_train, y_test):
-    #naiveBayes(X_train, X_test, y_train, y_test)
-    neuralNetwork100_0(X_train, X_test, y_train, y_test)
+def ejecucionModelos(X_train, X_test, y_train, y_test, x_final_test, y_final_test):
+    naiveBayes(X_train, X_test, y_train, y_test, x_final_test, y_final_test)
+    neuralNetwork(X_train, X_test, y_train, y_test, x_final_test, y_final_test)
 
 if __name__ == '__main__':
-    #file = 'Proyecto-Final-Inteligencia-Artificial/Data/0/Alegria_1.WAV'
-    file = 'Data/0/Asco_1.WAV'
     direc = 'Data'
+    x_final_test, y_final_test = getTestAudios(direc)
     X_entre, X_testeo, y_entre, y_testeo = parseo_datos(direc)
-    ejecucionModelos(X_entre, X_testeo, y_entre, y_testeo)
+    ejecucionModelos(X_entre, X_testeo, y_entre, y_testeo, x_final_test, y_final_test)
